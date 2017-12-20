f06b82889e35f45b1007ad438b3d3fe92f0eedaf
uvegla <uvegla@gmail.com>
2017-12-20 19:53:47 +0100

    Add solution for day 07 part 1

--- day_07/program.py
+++ day_07/program.py
@@ -1,12 +1,23 @@
 import re
 
+from anytree import Node
+
 
 def solve_part_1(puzzle_input):
-    return parse(puzzle_input)
+    return next(node.name for node in parse(puzzle_input).values() if node.is_root)
 
 
 def parse(puzzle_input):
-    nodes = [re.search(r'([a-z]*) \(([0-9]*)\)(?: -> )?([a-z, ]+)?.*', line).groups() for line in puzzle_input]
+    raw_nodes = [re.search(r'([a-z]*) \(([0-9]*)\)(?: -> )?([a-z, ]+)?.*', line).groups() for line in puzzle_input]
+
+    nodes = {}
+    for name, weight, _ in raw_nodes:
+        nodes[name] = Node(name, weight=weight)
+
+    for name, _, children in raw_nodes:
+        if children:
+            for child in children.split(', '):
+                nodes[child].parent = nodes[name]
 
     return nodes
 
--- day_07/tests.py
+++ day_07/tests.py
@@ -1,6 +1,24 @@
 from unittest import TestCase
 
+from program import solve_part_1
+
 
 class Day07TestCase(TestCase):
     def test_solve_part_1(self):
-        pass
+        lines = [
+            'pbga (66)',
+            'xhth (57)',
+            'ebii (61)',
+            'havc (66)',
+            'ktlj (57)',
+            'fwft (72) -> ktlj, cntj, xhth',
+            'qoyq (66)',
+            'padx (45) -> pbga, havc, qoyq',
+            'tknk (41) -> ugml, padx, fwft',
+            'jptl (61)',
+            'ugml (68) -> gyxo, ebii, jptl',
+            'gyxo (61)',
+            'cntj (57)'
+        ]
+
+        self.assertEqual(solve_part_1(lines), 'tknk')
