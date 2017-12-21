2229ebd04c22c170d3fe436cefb93394f28394ea
uvegla <uvegla@gmail.com>
2017-12-21 13:06:19 +0100

    Add solution for day 09 part 2

--- day_09/program.py
+++ day_09/program.py
@@ -1,5 +1,5 @@
-def solve_part_1(puzzle_input):
-    score, state, depth, pointer = 0, 'READ', 0, 0
+def solve_part_1_and_2(puzzle_input):
+    score, chars, state, depth, pointer = 0, 0, 'READ', 0, 0
 
     while pointer < len(puzzle_input):
         char = puzzle_input[pointer]
@@ -19,11 +19,13 @@ def solve_part_1(puzzle_input):
         elif state == 'DEVOUR':
             if char == '>':
                 state = 'READ'
+            else:
+                chars += 1
 
         pointer += 1
 
-    return score
+    return score, chars
 
 
 if __name__ == '__main__':
-    print solve_part_1(open('input.txt').readline())
+    print solve_part_1_and_2(open('input.txt').readline())
--- day_09/tests.py
+++ day_09/tests.py
@@ -1,29 +1,50 @@
 from unittest import TestCase
 
-from program import solve_part_1
+from program import solve_part_1_and_2
 
 
 class Day09TestCase(TestCase):
     def test_solve_part_1_example_1(self):
-        self.assertEqual(solve_part_1('{}'), 1)
+        self.assertEqual(solve_part_1_and_2('{}')[0], 1)
 
     def test_solve_part_1_example_2(self):
-        self.assertEqual(solve_part_1('{{{}}}'), 6)
+        self.assertEqual(solve_part_1_and_2('{{{}}}')[0], 6)
 
     def test_solve_part_1_example_3(self):
-        self.assertEqual(solve_part_1('{{},{}}'), 5)
+        self.assertEqual(solve_part_1_and_2('{{},{}}')[0], 5)
 
     def test_solve_part_1_example_4(self):
-        self.assertEqual(solve_part_1('{{{},{},{{}}}}'), 16)
+        self.assertEqual(solve_part_1_and_2('{{{},{},{{}}}}')[0], 16)
 
     def test_solve_part_1_example_5(self):
-        self.assertEqual(solve_part_1('{<a>,<a>,<a>,<a>}'), 1)
+        self.assertEqual(solve_part_1_and_2('{<a>,<a>,<a>,<a>}')[0], 1)
 
     def test_solve_part_1_example_6(self):
-        self.assertEqual(solve_part_1('{{<ab>},{<ab>},{<ab>},{<ab>}}'), 9)
+        self.assertEqual(solve_part_1_and_2('{{<ab>},{<ab>},{<ab>},{<ab>}}')[0], 9)
 
     def test_solve_part_1_example_7(self):
-        self.assertEqual(solve_part_1('{{<!!>},{<!!>},{<!!>},{<!!>}}'), 9)
+        self.assertEqual(solve_part_1_and_2('{{<!!>},{<!!>},{<!!>},{<!!>}}')[0], 9)
 
     def test_solve_part_1_example_8(self):
-        self.assertEqual(solve_part_1('{{<a!>},{<a!>},{<a!>},{<ab>}}'), 3)
+        self.assertEqual(solve_part_1_and_2('{{<a!>},{<a!>},{<a!>},{<ab>}}')[0], 3)
+
+    def test_solve_part_2_example_1(self):
+        self.assertEqual(solve_part_1_and_2('<>')[1], 0)
+
+    def test_solve_part_2_example_2(self):
+        self.assertEqual(solve_part_1_and_2('<random characters>')[1], 17)
+
+    def test_solve_part_2_example_3(self):
+        self.assertEqual(solve_part_1_and_2('<<<<>')[1], 3)
+
+    def test_solve_part_2_example_4(self):
+        self.assertEqual(solve_part_1_and_2('<{!>}>')[1], 2)
+
+    def test_solve_part_2_example_5(self):
+        self.assertEqual(solve_part_1_and_2('<!!>')[1], 0)
+
+    def test_solve_part_2_example_6(self):
+        self.assertEqual(solve_part_1_and_2('<!!!>>')[1], 0)
+
+    def test_solve_part_2_example_7(self):
+        self.assertEqual(solve_part_1_and_2('<{o"i!a,<{i<a>')[1], 10)
