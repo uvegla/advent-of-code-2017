7fc976bcc6a53300631085a918d587d57b567025
uvegla <uvegla@gmail.com>
2017-12-27 15:12:00 +0100

    Add solution for day 10 part 2

--- day_10/program.py
+++ day_10/program.py
@@ -1,27 +1,49 @@
-def solve_part_1(r, puzzle_input):
-    current, skip = 0, 0
+from functools import reduce
 
-    for length in puzzle_input:
-        (s1_s, s1_e), (s2_s, s2_e) = get_sections(r, current, length)
 
-        reverse = (r[s1_s:s1_e] + r[s2_s:s2_e])[::-1]
+def solve_part_1(sparse_hash, lengths):
+    sparse_hash, _, _ = single_round(sparse_hash, lengths)
 
-        r[s1_s:s1_e] = reverse[:s1_e-s1_s]
-        r[s2_s:s2_e] = reverse[s1_e-s1_s:]
+    return sparse_hash[0] * sparse_hash[1]
 
-        current = (current + length + skip) % len(r)
+
+def single_round(sparse_hash, lengths, current=0, skip=0):
+    for length in lengths:
+        (s1_s, s1_e), (s2_s, s2_e) = get_sections(sparse_hash, current, length)
+
+        reverse = (sparse_hash[s1_s:s1_e] + sparse_hash[s2_s:s2_e])[::-1]
+
+        sparse_hash[s1_s:s1_e] = reverse[:s1_e - s1_s]
+        sparse_hash[s2_s:s2_e] = reverse[s1_e - s1_s:]
+
+        current = (current + length + skip) % len(sparse_hash)
         skip += 1
 
-    return r[0] * r[1]
+    return sparse_hash, current, skip
 
 
-def get_sections(r, current, length):
-    if current + length < len(r):
+def get_sections(sparse_hash, current, length):
+    if current + length < len(sparse_hash):
         return (current, current + length), (0, 0)
     else:
-        return (current, len(r)), (0,  current + length - len(r))
+        return (current, len(sparse_hash)), (0, current + length - len(sparse_hash))
+
+
+def solve_part_2(puzzle_input):
+    lengths = [ord(c) for c in puzzle_input] + [17, 31, 73, 47, 23]
+
+    sparse_hash, current, skip = range(256), 0, 0
+    for _ in range(64):
+        sparse_hash, current, skip = single_round(sparse_hash, lengths, current, skip)
+
+    dense_hash = [reduce(lambda x, y: x ^ y, sparse_hash[(i * 16):((i + 1) * 16)]) for i in range(16)]
+
+    return ''.join(['{:02x}'.format(c) for c in dense_hash])
 
 
 if __name__ == '__main__':
-    lengths = [int(x) for x in open('input.txt').readline().split(',')]
+    line = open('input.txt').readline()
+    lengths = [int(x) for x in line.split(',')]
+
     print solve_part_1(range(256), lengths)
+    print solve_part_2(line)
--- day_10/tests.py
+++ day_10/tests.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from program import solve_part_1, get_sections
+from program import get_sections, solve_part_1, solve_part_2
 
 
 class Day10TestCase(TestCase):
@@ -24,3 +24,15 @@ class Day10TestCase(TestCase):
 
     def test_solve_part_1(self):
         self.assertEqual(solve_part_1(range(5), [3, 4, 1, 5]), 12)
+
+    def test_solve_part_2_example_1(self):
+        self.assertEqual(solve_part_2(''), 'a2582a3a0e66e6e86e3812dcb672a272')
+
+    def test_solve_part_2_example_2(self):
+        self.assertEqual(solve_part_2('AoC 2017'), '33efeb34ea91902bb2f59c9920caa6cd')
+
+    def test_solve_part_2_example_3(self):
+        self.assertEqual(solve_part_2('1,2,3'), '3efbe78a8d82f29979031a4aa0b16a9d')
+
+    def test_solve_part_2_example_4(self):
+        self.assertEqual(solve_part_2('1,2,4'), '63960835bcdc130f0b66d7ff4f6a5a8e')
