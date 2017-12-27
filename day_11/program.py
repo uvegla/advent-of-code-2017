141969078afd3cf3ecb9c7ca6fc562b17bb8eaf8
uvegla <uvegla@gmail.com>
2017-12-27 16:04:48 +0100

    Add solution for day 11 part 2

--- day_11/program.py
+++ day_11/program.py
@@ -1,5 +1,3 @@
-from functools import reduce
-
 DIRECTIONS = {
     'n': (-1, 1),
     'ne': (0, 1),
@@ -10,12 +8,25 @@ DIRECTIONS = {
 }
 
 
-def solve_part_1(puzzle_input):
-    position = reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), [DIRECTIONS[x] for x in puzzle_input])
+def solve_part_1_and_2(puzzle_input):
+    max_distance, position = 0, (0, 0)
 
+    for direction in puzzle_input:
+        position = step(position, DIRECTIONS[direction])
+        max_distance = max(max_distance, distance(position))
+
+    return distance(position), max_distance
+
+
+def step(x, y):
+    return x[0] + y[0], x[1] + y[1]
+
+
+def distance(position):
     return max(abs(position[0]), abs(position[1]))
 
 
 if __name__ == '__main__':
     line = open('input.txt').readline().split(',')
-    print solve_part_1(line)
+
+    print solve_part_1_and_2(line)
--- day_11/tests.py
+++ day_11/tests.py
@@ -1,17 +1,17 @@
 from unittest import TestCase
 
-from program import solve_part_1
+from program import solve_part_1_and_2
 
 
 class Day11TestCase(TestCase):
     def test_solve_part_1_example_1(self):
-        self.assertEqual(solve_part_1(['ne', 'ne', 'ne']), 3)
+        self.assertEqual(solve_part_1_and_2(['ne', 'ne', 'ne'])[0], 3)
 
     def test_solve_part_1_example_2(self):
-        self.assertEqual(solve_part_1(['ne', 'ne', 'sw', 'sw']), 0)
+        self.assertEqual(solve_part_1_and_2(['ne', 'ne', 'sw', 'sw'])[0], 0)
 
     def test_solve_part_1_example_3(self):
-        self.assertEqual(solve_part_1(['ne', 'ne', 's', 's']), 2)
+        self.assertEqual(solve_part_1_and_2(['ne', 'ne', 's', 's'])[0], 2)
 
     def test_solve_part_1_example_4(self):
-        self.assertEqual(solve_part_1(['se', 'sw', 'se', 'sw', 'sw']), 3)
+        self.assertEqual(solve_part_1_and_2(['se', 'sw', 'se', 'sw', 'sw'])[0], 3)
