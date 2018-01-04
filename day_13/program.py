d35f3b0460024af0bdef27cf55f67ade40322139
uvegla <uvegla@gmail.com>
2018-01-04 16:14:16 +0100

    Add solution for day 13 part 2

--- day_13/program.py
+++ day_13/program.py
@@ -1,14 +1,15 @@
 import re
+import itertools
 
 
-def solve_part_1(puzzle_input):
-    severity = 0
+def generic(puzzle_input, delay=0):
+    apprehends = []
 
     for depth in range(max(puzzle_input.keys()) + 1):
-        if depth in puzzle_input and collides(depth, puzzle_input[depth]):
-            severity += depth * puzzle_input[depth]
+        if depth in puzzle_input and collides(depth + delay, puzzle_input[depth]):
+            apprehends.append((depth, puzzle_input[depth]))
 
-    return severity
+    return apprehends
 
 
 def collides(depth, rng):
@@ -19,5 +20,18 @@ def parse(lines):
     return {int(depth): int(rng) for (depth, rng) in [re.search(r'(\d+): (\d+)', line).groups() for line in lines]}
 
 
+def solve_part_1(puzzle_input):
+    return sum([depth * rng for (depth, rng) in generic(puzzle_input)])
+
+
+def solve_part_2(puzzle_input):
+    for i in itertools.count():
+        if len(generic(puzzle_input, i)) == 0:
+            return i
+
+
 if __name__ == '__main__':
-    print solve_part_1(parse(open('input.txt').readlines()))
+    parsed_input = parse(open('input.txt').readlines())
+
+    print solve_part_1(parsed_input)
+    print solve_part_2(parsed_input)
--- day_13/tests.py
+++ day_13/tests.py
@@ -1,8 +1,11 @@
 from unittest import TestCase
 
-from program import solve_part_1
+from program import solve_part_1, solve_part_2
 
 
 class Day13TestCase(TestCase):
     def test_solve_part_1(self):
         self.assertEqual(solve_part_1({0: 3, 1: 2, 4: 4, 6: 4}), 24)
+
+    def test_solve_part_2(self):
+        self.assertEqual(solve_part_2({0: 3, 1: 2, 4: 4, 6: 4}), 10)
