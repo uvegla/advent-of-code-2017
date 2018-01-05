469109fd9295fe991d84ef87dcfa845615a43d00
uvegla <uvegla@gmail.com>
2018-01-05 14:11:13 +0100

    Add solution for day 15 part 2

--- day_15/program.py
+++ day_15/program.py
@@ -2,7 +2,7 @@ def solve_part_1(init_a, init_b):
     generator_a = generator(init_a, 16807)
     generator_b = generator(init_b, 48271)
 
-    return sum([1 if l16m(next(generator_a), next(generator_b)) else 0 for _ in range(4 * 10 ** 7)])
+    return compare_n_times(generator_a, generator_b, 4 * 10 ** 7)
 
 
 def generator(initial, factor):
@@ -16,5 +16,27 @@ def l16m(a, b):
     return '{0:b}'.format(a)[-16:] == '{0:b}'.format(b)[-16:]
 
 
+def solve_part_2(init_a, init_b):
+    generator_a = generator_with_criteria(generator(init_a, 16807), 4)
+    generator_b = generator_with_criteria(generator(init_b, 48271), 8)
+
+    return compare_n_times(generator_a, generator_b, 5 * 10 ** 6)
+
+
+def generator_with_criteria(raw_generator, divisor):
+    while True:
+        value = next(raw_generator)
+
+        if value % divisor == 0:
+            yield value
+        else:
+            continue
+
+
+def compare_n_times(generator_a, generator_b, times):
+    return sum([1 if l16m(next(generator_a), next(generator_b)) else 0 for _ in range(times)])
+
+
 if __name__ == '__main__':
     print solve_part_1(289, 629)
+    print solve_part_2(289, 629)
--- day_15/tests.py
+++ day_15/tests.py
@@ -1,8 +1,11 @@
 from unittest import TestCase
 
-from program import solve_part_1
+from program import solve_part_1, solve_part_2
 
 
 class Day15TestCase(TestCase):
     def test_solve_part_1(self):
         self.assertEqual(solve_part_1(65, 8921), 588)
+
+    def test_solve_part_2(self):
+        self.assertEqual(solve_part_2(65, 8921), 309)
