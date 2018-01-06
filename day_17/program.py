3f27b2777b5563bb686c7a3957e589eb58137be7
uvegla <uvegla@gmail.com>
2018-01-07 00:24:51 +0100

    Add solution for day 17 part 2

--- day_17/program.py
+++ day_17/program.py
@@ -1,15 +1,26 @@
 from collections import deque
 
 
-def solve_part_1(steps):
-    circular_buffer = deque()
+def generic(steps, length):
+    circular_buffer = deque(maxlen=length)
 
-    for i in xrange(2018):
+    for i in xrange(length):
         circular_buffer.rotate(steps)
         circular_buffer.appendleft(i)
 
-    return circular_buffer[-1]
+    return circular_buffer
+
+
+def solve_part_1(steps):
+    return generic(steps, 2018)[-1]
+
+
+def solve_part_2(steps):
+    circular_buffer = list(generic(steps, 5 * 10 ** 7 + 1))
+
+    return circular_buffer[circular_buffer.index(0) - 1]
 
 
 if __name__ == '__main__':
     print solve_part_1(366)
+    print solve_part_2(366)
